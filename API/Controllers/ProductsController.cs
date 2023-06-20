f065f7b3050082160cf8160d8a9df0e1b001ac5e
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2023-06-20 15:28:40 +0200

    .

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,7 +1,8 @@
-using API.Data;
-using API.Entities;
+using Infrastructure.Data;
+using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -9,11 +10,11 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repo;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;   
+            _repo = repo;
         }
 
         // endpoints
@@ -21,13 +22,13 @@ namespace API.Controllers
         public async Task <ActionResult<List<Product>>> GetProducts(){ 
             // use "Task" to make it async.
             // This prevents the waiting of requests. Cuncurrent requests.
-            var products = await _context.Products.ToListAsync();
-            return products;
+            var products = await _repo.GetProductsAsync();
+            return Ok(products);
         }
 
         [HttpGet("{id}")] // get the id from the request url
         public async Task<ActionResult<Product>> GetProduct(int id){ // access the id in the controller
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
-using API.Data;
+using Core.Interfaces;
+using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StoreContext>(opt =>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 
 var app = builder.Build();
